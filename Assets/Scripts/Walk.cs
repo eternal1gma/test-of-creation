8c06714a0481fdf08b041fefb1be063f47b711e9
Jurish De Leon <jurishd@live.wsd1.org>
2026-02-19 14:36:09 -0600

    inital commit for remaining files

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    [SerializeField] private int speed;
+    [SerializeField] private Transform theCamera;
+    [SerializeField] private Vector3 previousPosition; //value type
+    // Start is called before the first frame update
+    void Start()
+    {
+        // hell part2 let's go
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward);
+    }
+}
